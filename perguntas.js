73649cbf552afdc1fd8240cb5a8ee02647331531
caio <andrini.caio@escola.pr.gov.br>
2025-05-29 11:04:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript ?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Alemanha ?',
+    'A capital da Alemanha é Berlim'
+)
+
+criaCartao(
+    'Química',
+    'Qual o PH da água ?',
+    '7'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
